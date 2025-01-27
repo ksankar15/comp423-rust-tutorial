dba859eed7816903f595d64c5fc4bb958aac1693
Kavin Sankar <cary.sankars@gmail.com>
2025-01-27 02:19:54 +0000

    Hello World in Rust

---
+++ hello-world/target/debug/deps/hello_world-458bc23ec5b9ef2e.d
@@ -0,0 +1,5 @@
+/workspaces/rust-tutorial/hello-world/target/debug/deps/hello_world-458bc23ec5b9ef2e: src/main.rs
+
+/workspaces/rust-tutorial/hello-world/target/debug/deps/hello_world-458bc23ec5b9ef2e.d: src/main.rs
+
+src/main.rs:
---
+++ hello-world/target/debug/hello-world.d
@@ -0,0 +1,1 @@
+/workspaces/rust-tutorial/hello-world/target/debug/hello-world: /workspaces/rust-tutorial/hello-world/src/main.rs
